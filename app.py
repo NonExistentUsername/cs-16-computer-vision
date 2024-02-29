d14a7340f11b186363e3d300f5742e95ab4cd586
Dmytro Maior <mayor4090Gmail.com>
2024-02-29 03:11:42 +0200

    Big update

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-import random
-import threading
 import time
+from multiprocessing import Manager, Pool, Process
 
 import cv2
-import keyboard  # type: ignore
 import numpy as np
-import pyautogui  # type: ignore
 from mss import mss
 
 from predictions import model, predict
 
-bounding_box = {"top": 60, "left": 0, "width": 1600, "height": 600}
+# import Value from multiprocessing
+
+
+bounding_box = {"top": 60, "left": 0, "width": 1600, "height": 1200}
+# bounding_box = {"top": 60, "left": 0, "width": 2389, "height": 1024}
 
 cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
+
+image_size = 1024
+image_sizes = (image_size, image_size)
 
 sct = mss()
 
 auto_shoot = False
+conf_limit = 0.5
 
-conf_limit = 0.45
+# image_predictions: deque = deque(maxlen=1)
 
 
 def destroy():
@@ -27,17 +32,26 @@ def destroy():
 
 
 def add_print_fps(draw_func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         start = time.time()
-        draw_func()
+        sct_img, image_predictions = draw_func(*args, **kwargs)
         delta = time.time() - start
-        print(f"FPS: {1 / delta:.2f}\nTime: {delta * 1000:.2f} ms")
+        cv2.putText(
+            sct_img,
+            f"FPS: {1 / delta:.2f}",
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2,
+            cv2.LINE_AA,
+        )
+        return sct_img, image_predictions
 
     return wrapper
 
 
 class_names = {0: "ct_body", 1: "ct_head", 2: "t_body", 3: "t_head"}
-print(model.names)
 colors = [(0, 0, 255), (126, 126, 255), (255, 126, 126), (255, 255, 0)]
 
 
@@ -51,7 +65,14 @@ def draw_box(img, data):
 
 
 def draw_predictions(img, prediction):
-    for box in prediction.boxes:
+    for box in prediction:
+        xmin, ymin, xmax, ymax, color = box
+        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), colors[color], 2)
+
+
+def process_predictions(predictions):
+    boxes = []
+    for box in predictions.boxes:
         if box.conf < conf_limit:
             continue
 
@@ -61,45 +82,38 @@ def draw_predictions(img, prediction):
         xmax = int(data[2])
         ymax = int(data[3])
 
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), colors[int(box.cls)], 2)
+        boxes.append((xmin, ymin, xmax, ymax, int(box.cls)))
+
+    return boxes
 
 
-img_predictions = None
-
-
-def predict_in_background():
-    global img_predictions
+def predict_in_background(image_predictions):
     while True:
         sct_img = sct.grab(bounding_box)
         sct_img = np.array(sct_img)
-        sct_img = cv2.resize(sct_img, (640, 640))
+        sct_img = cv2.resize(sct_img, image_sizes)
         sct_img = cv2.cvtColor(sct_img, cv2.COLOR_BGR2RGB)
-        img_predictions = predict(sct_img)
+        predictions = predict(sct_img)
+        processed_predictions = process_predictions(predictions)
+        image_predictions.put(processed_predictions)
 
 
-def start_predictions():
-    import threading
-
-    t = threading.Thread(target=predict_in_background, daemon=True)
-    t.start()
-
-
-# @add_print_fps
-def draw():
-    global img_predictions
+@add_print_fps
+def draw(image_predictions):
+    image_predictions = image_predictions.get()
     sct_img = sct.grab(bounding_box)
     sct_img = np.array(sct_img)
-    sct_img = cv2.resize(sct_img, (640, 640))
-    draw_predictions(sct_img, img_predictions)
-    cv2.imshow("screen", sct_img)
+    sct_img = cv2.resize(sct_img, image_sizes)
+    draw_predictions(sct_img, image_predictions)
+    return sct_img, image_predictions
 
 
 def window_position_to_real_position(x, y):
     window_x, window_y = bounding_box["left"], bounding_box["top"]
     window_width, window_height = bounding_box["width"], bounding_box["height"]
 
-    x = x / 640 * window_width
-    y = y / 640 * window_height
+    x = x / image_size * window_width
+    y = y / image_size * window_height
 
     x += window_x
     y += window_y
@@ -107,67 +121,8 @@ def window_position_to_real_position(x, y):
     return x, y
 
 
-def move_mouse(x, y):
-    x, y = window_position_to_real_position(x, y)
-    pyautogui.moveTo(x, y, duration=0.01)
-    # pyautogui.click()
-
-
 def get_mouse_position():
-    return 640 / 2, 640 / 2
-
-
-def get_closest_box(x, y):
-    closest_box = None
-    is_head = False
-    closest_distance = float("inf")
-
-    for box in img_predictions.boxes:
-        if box.conf < conf_limit:
-            continue
-
-        if is_head and (int(box.cls) == 0 or int(box.cls) == 2):
-            continue
-
-        if is_head == False and (int(box.cls) == 1 or int(box.cls) == 3):
-            is_head = True
-            closest_box = box
-            continue
-
-        data = box.data[0]
-        xmin = int(data[0])
-        ymin = int(data[1])
-        xmax = int(data[2])
-        ymax = int(data[3])
-
-        distance = (x - (xmin + xmax) / 2) ** 2 + (y - (ymin + ymax) / 2) ** 2
-
-        if distance < closest_distance:
-            closest_distance = distance
-            closest_box = box
-
-    max_distance = 60**2
-
-    if closest_distance > max_distance:
-        return None
-
-    return closest_box
-
-
-def move_mouse_to_closest_box(x, y):
-    box = get_closest_box(x, y)
-
-    if box is not None:
-        data = box.data[0]
-        xmin = int(data[0])
-        ymin = int(data[1])
-        xmax = int(data[2])
-        ymax = int(data[3])
-
-        x = (xmin + xmax) / 2
-        y = (ymin + ymax) / 2
-
-        move_mouse(x, y)
+    return 1024 / 2, 1024 / 2
 
 
 def move_mouse_to_closest_box_in_background():
@@ -185,45 +140,27 @@ def run():
 
     stop = False
 
-    for _ in range(3):
-        time.sleep(0.1)
-        start_predictions()
+    # Create a manager for the deque and start a server
+    with Manager() as manager:
+        image_predictions = manager.Queue(maxsize=4)
 
-    while img_predictions is None:
-        print("Waiting for predictions")
-        time.sleep(0.1)
+        # with manager.Pool(1) as p:
+        # p.apply_async(predict_in_background, args=[image_predictions])
+        for _ in range(2):
+            p = Process(target=predict_in_background, args=[image_predictions])
+            p.start()
 
-    last_shoot = time.time()
-    last_auto_shoot_change = time.time()
+        last_shoot = time.time()
+        last_auto_shoot_change = time.time()
 
-    while not stop:
-        draw()
+        while not stop:
+            sct_img, boxes = draw(image_predictions)
+            cv2.imshow("screen", sct_img)
 
-        if (cv2.waitKey(1) & 0xFF) == ord("q"):
-            stop = True
+            if (cv2.waitKey(1) & 0xFF) == ord("q"):
+                stop = True
 
-        if (
-            keyboard.is_pressed("command")
-            and time.time() - last_auto_shoot_change > 0.5
-        ):
-            auto_shoot = not auto_shoot
-
-            if auto_shoot:
-                print("Auto shoot enabled")
-            else:
-                print("Auto shoot disabled")
-
-            last_auto_shoot_change = time.time()
-
-        if auto_shoot and time.time() - last_shoot > 0.1:
-            t = threading.Thread(
-                target=lambda: move_mouse_to_closest_box(*get_mouse_position()),
-                daemon=True,
-            )
-            t.start()
-            last_shoot = time.time()
-
-    destroy()
+        destroy()
 
 
 if __name__ == "__main__":
